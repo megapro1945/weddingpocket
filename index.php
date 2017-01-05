aa453a69db6cda2a2b7283cc85a4902e4b64c6bc
megapro1945 <weddingpocket2016@gmail.com>
2017-01-05 18:21:03 +0700

    'assets'

---
+++ index.php
@@ -0,0 +1,258 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="img/icon.png">
+
+	<title>Wedding Pocket</title>
+
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/jumbotron.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/sosial-media.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	
+</head>
+
+<body>
+	<div class="home-header">
+		<div class="overlay"></div>
+
+		<nav class="navbar navbar-static-top navbar-dark">
+	      <a class="navbar-brand" id="e" href="index.php">
+	      	<img class="logo" src="img/logo2.png">
+	      	<img class="logo-text" src="img/tulisan.png">
+	      </a>
+	    </nav>
+
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="jumbotron">
+	        <div class="jumbotron-text">
+	        	<h1>Rencanakan Pernikahan Anda Sekarang</h1>
+	        </div>
+	        <div>
+	        	<form class="form-inline form-budget">
+	        		<div class="form-group">
+	        			<input type="text" class="form-control budget-field" name="budget" placeholder="Masukkan Budget Anda">
+	        		</div>
+	        		<button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#demo-modal-3">Mulai Sekarang</button>
+	        	</form>
+	        </div>
+	    </div>
+    </div>
+
+
+    <div class="container">
+	    <div class="why-wedding-pocket-header">
+	    	<h2>Mengapa Wedding Pocket?</h2>
+	    </div>
+
+		<!-- Example row of columns -->
+		<div class="row">
+			<div class="col-md-4">
+			  <img class="img-fluid advisor" src="img/icon/advisor.png">
+			  <h4 class="why-text">Smart Wedding</h4>
+			  <p>Menambah kenyamanan anda dalam mempersiapkan konsep wedding impian anda.</p>
+			</div>
+			<div class="col-md-4">
+			  <img class="img-fluid advisor" src="img/icon/advisor.png">
+			  <h4 class="why-text">Smart Wedding</h4>
+			  <p>Menambah kenyamanan anda dalam mempersiapkan konsep wedding impian anda.</p>
+			</div>
+			<div class="col-md-4">
+			  <img class="img-fluid advisor" src="img/icon/advisor.png">
+			  <h4 class="why-text">Smart Wedding</h4>
+			  <p>Menambah kenyamanan anda dalam mempersiapkan konsep wedding impian anda.</p>
+			</div>
+		</div>
+
+     <!--  <hr>
+
+      <footer>
+        <p>&copy; Wedding Pocket 2017</p>
+      </footer> -->
+    </div> <!-- /container -->
+
+    <hr>
+
+    <div class="container">
+    	<div class="patner-kami-header">
+	    	<h2>Partner Kami</h2>
+	    </div>
+
+		<!-- Example row of columns -->
+		<div class="row">
+			<div class="col-md-4">
+			  <div class="card">
+				  <img class="card-img-top" src="img/konten/konten.jpg" alt="patner-1">
+				  <div class="card-block">
+				    <h4 class="card-title">Kana Entertainment</h4>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+			   </div>
+			</div>
+			<div class="col-md-4">
+			  <div class="card">
+				  <img class="card-img-top" src="img/konten/konten-2.jpg" alt="patner-2">
+				  <div class="card-block">
+				    <h4 class="card-title">Truly Entertainment</h4>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+			   </div>
+			</div>
+			<div class="col-md-4">
+			  <div class="card">
+				  <img class="card-img-top" src="img/konten/konten.jpg" alt="patner-1">
+				  <div class="card-block">
+				    <h4 class="card-title">Card title</h4>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+			   </div>
+			</div>
+		</div>
+
+		
+    </div> <!-- /container -->
+
+	
+
+	<div class="container-fluid bg-header-footer">
+		<div class="container bg-body">
+			<div class="row bottom">
+				<div class="col-xs-6 col-sm-3">
+					<h3 class="footer-about-us">WEDDING POCKET</h3>
+						<li class="footer-about-us-text"><a href="">Tentang Kami</a></li>
+						<li class="footer-about-us-text"><a href="">Karir</a></li>
+						<li class="footer-about-us-text"><a href="">Hubungi Kami</a></li>
+				</div>
+			    <div class="col-xs-6 col-sm-3">
+			    	<h3 class="footer-about-us">PRODUK</h3>
+						<li class="footer-about-us-text"><a href="">Smart Wedding</a></li>
+						<li class="footer-about-us-text"><a href=""></a></li>
+						<li class="footer-about-us-text"><a href=""></a></li>
+			    </div>
+
+			    <div class="clearfix hidden-sm-up"></div>
+
+			    <div class="col-xs-6 col-sm-3">
+			    	<h3 class="footer-about-us">BANTUAN</h3>
+						<li class="footer-about-us-text"><a href="">FAQ</a></li>
+						<li class="footer-about-us-text"><a href="">Beri Masukkan</a></li>
+			    </div>
+			    <div class="col-xs-6 col-sm-3">
+			    	<h3 class="footer-about-us">CUSTOMER SERVICE</h3>
+						<li class="footer-about-us-text-cs">0821373273723</li>
+						<li class="footer-about-us-text-cs">cs@weddingpocket.net</li>
+						<li class="footer-about-us-text-cs">Senin-Jumat (08.00 - 18.00)</li>
+						<div class="sosial-media-icon">
+		                    <ul class="social-network social-circle">
+		                        <li>
+		                        	<a href="#" class="icoFacebook sosmed" title="Facebook">
+		                        		<i class="fa fa-facebook"></i>
+		                        	</a>
+		                        </li>
+		                        <li>
+		                        	<a href="#" class="icoInstagram sosmed" title="Instagram">
+		                        		<i class="fa fa-instagram"></i>
+		                        	</a>
+		                        </li>
+		                        <li>
+		                        	<a href="#" class="icoTwitter sosmed" title="Twitter">
+		                        		<i class="fa fa-twitter"></i>
+		                        	</a>
+		                        </li>
+		                        <li>
+		                        	<a href="#" class="icoGoogle sosmed" title="Google +">
+		                        		<i class="fa fa-google-plus"></i>
+		                        	</a>
+		                        </li>
+		                    </ul>				
+						</div>
+			    </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="footer-logo">
+				<img class="logo" src="img/logo2.png">
+			</div>
+			<div class="col-xs-6 col-sm-10 footer-copyright">
+				<footer>
+			    	<p>&copy; Copyright 2017. PT WeddingPocket. All rights reserved.</p>
+			  	</footer>
+			</div>
+		</div>
+		
+	</div>
+
+	<!-- Modal -->
+	<form class="modal multi-step" id="demo-modal-3">
+	    <div class="modal-dialog">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4 class="modal-title step-1" data-step="1">Step 1</h4>
+	                <h4 class="modal-title step-2" data-step="2">Step 2</h4>
+	                <h4 class="modal-title step-3" data-step="3">Final Step</h4>
+	                <div class="m-progress">
+	                    <div class="m-progress-bar-wrapper">
+	                        <div class="m-progress-bar">
+	                        </div>
+	                    </div>
+	                    <div class="m-progress-stats">
+	                        <span class="m-progress-current">
+	                        </span>
+	                        /
+	                        <span class="m-progress-total">
+	                        </span>
+	                    </div>
+	                    <div class="m-progress-complete">
+	                        Completed
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="modal-body step-1" data-step="1">
+	                This is step 1.
+	            </div>
+	            <div class="modal-body step-2" data-step="2">
+	                This is the second step.
+	            </div>
+	            <div class="modal-body step-3" data-step="3">
+	                This is the final step.
+	            </div>
+	            <div class="modal-footer">
+	                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	                <button type="button" class="btn btn-primary step step-1" data-step="1" onclick="sendEvent('#demo-modal-3', 2)">Continue</button>
+	                <button type="button" class="btn btn-primary step step-2" data-step="2" onclick="sendEvent('#demo-modal-3', 1)">Back</button>
+	                <button type="button" class="btn btn-primary step step-2" data-step="2" onclick="sendEvent('#demo-modal-3', 3)">Continue</button>
+	            </div>
+	        </div>
+	    </div>
+	</form>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+ 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    
+    <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="js/js-lama/bootstrap.min.js"></script> -->
+    <script type="text/javascript" src="js/multi-step-modal.js"></script>
+    <script>
+		sendEvent = function(sel, step) {
+		    $(sel).trigger('next.m.' + step);
+		}
+	</script>
+    
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+
+</html> 
